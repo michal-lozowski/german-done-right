ac7f06bc0486ee9a63263c511d136c3212a8bfa7
Michal Lozowski <michal.lozowski@proton.me>
2023-06-14 21:26:26 +0200

    noname

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
-let questions = [];
-let answers = [];
+// script.js
 
-// Read the questions from the questions.txt file
-fetch('questions.txt')
+window.addEventListener('DOMContentLoaded', (event) => {
+  // Load the questions and answers from the files
+  fetch('questions.txt')
     .then(response => response.text())
-    .then(questionsText => {
-        // Replace line breaks with <br> tags
-        questionsText = questionsText.replace(/\n/g, '<br>');
+    .then(questions => {
+      fetch('answers.txt')
+        .then(response => response.text())
+        .then(answers => {
+          // Split the questions and answers into arrays
+          const questionArray = questions.split('_');
+          const answerArray = answers.split(',');
 
-        // Split the questions into an array
-        questions = questionsText.split('_');
+          // Get the container element to add the questions
+          const container = document.getElementById('test-container');
+
+          // Loop through the questions and create input fields
+          for (let i = 0; i < questionArray.length; i++) {
+            // Create a new input field
+            const input = document.createElement('input');
+            input.type = 'text';
+
+            // Set up event listener for the Enter key press
+            input.addEventListener('keydown', (event) => {
+              if (event.key === 'Enter') {
+                event.preventDefault(); // Prevent form submission
+
+                // Check if the answer is correct
+                const userAnswer = input.value.trim();
+                const correctAnswer = answerArray[i].trim();
+                if (userAnswer === correctAnswer) {
+                  input.classList.remove('incorrect');
+                  input.classList.add('correct');
+                } else {
+                  input.classList.remove('correct');
+                  input.classList.add('incorrect');
+                }
+
+                // Move focus to the next input field
+                const nextInput = document.querySelector('input:not(.correct):not(.incorrect)');
+                if (nextInput) {
+                  nextInput.focus();
+                }
+              }
+            });
+
+            // Add the input field to the container
+            container.appendChild(input);
+
+            // Add the question text after the input field
+            const questionText = document.createTextNode(questionArray[i]);
+            container.appendChild(questionText);
+          }
+        })
+        .catch(error => {
+          console.error('Error loading answers:', error);
+        });
+    })
+    .catch(error => {
+      console.error('Error loading questions:', error);
+    });
 });
-
-// Read the answers from the answers.txt file
-fetch('answers.txt')
-      .then(response => response.text())
-      .then(answersText => {
-        // Split the answers into an array
-        answers = answersText.split(',');
-});
-
-// Function to generate the HTML for fill in the gaps exercise
-function generateExerciseHTML() {
- 
-  var exerciseHTML = [];
-  
-  for (var i = 0; i < questions.length; i++) {
-    exerciseHTML += questions[i];
-  }
-  
-  document.getElementById("exercise-container").innerHTML = exerciseHTML;
-}
-
-// Call the function to generate the exercise HTML
-generateExerciseHTML();
