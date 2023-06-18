cc9a51a43f957debd02afee262496920b2450d7f
Michal Lozowski <michal.lozowski@proton.me>
2023-06-18 19:29:38 +0200

    noname

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 fetch('questions.txt')
   .then(response => response.text())
   .then(questionsText => {
-    questionsText = questionsText.replace("\n", "<br>")
+    questionsText = questionsText.replace("\n","<br>")
     const questions = questionsText.trim().split('_');
       
     container = document.getElementById('test-container');
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < questions.length; i++) {
       const text = document.createTextNode(questions[i]);
       container.appendChild(text);
       const input = document.createElement('input');
-      const uniqueId = 'input_' + i; // Unique ID for each input field 
+      const uniqueId = 'input_' + i; d 
       input.setAttribute('type', 'text');
-      input.setAttribute('id', uniqueId);  
+      input.setAttribute('id', uniqueId);
+      input.setAttribute('class', "answerField");   
       container.appendChild(input);
   };
 
