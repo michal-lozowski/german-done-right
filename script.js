eb2fa3247ef477058d29f3cf7e3aa78b1598610a
Michal Lozowski <michal.lozowski@proton.me>
2023-06-18 19:13:54 +0200

    noname

--- script.js
+++ script.js
@@ -3,22 +3,23 @@ fetch('questions.txt')
   .then(questionsText => {
     questionsText = questionsText.replace("\n", "<br>")
     const questions = questionsText.trim().split('_');
+      
+    container = document.getElementById('test-container');
+    for (let i = 0; i < 10; i++) {
+      const text = document.createTextNode(questions[i]);
+      container.appendChild(text);
+      const input = document.createElement('input');
+      const uniqueId = 'input_' + i; // Unique ID for each input field 
+      input.setAttribute('type', 'text');
+      input.setAttribute('id', uniqueId);  
+      container.appendChild(input);
+  };
+
   });
 
 fetch('answers.txt')
   .then(response => response.text())
   .then(answersText => {
     const lines = answersText.split('\n');
-    const array2D = lines.map(line => line.split(','));
+    const answers = lines.map(line => line.split(','));
   });
-  
-container = document.getElementById('test-container');
-for (let i = 0; i < 10; i++) {
-  const text = document.createTextNode(questions[i]);
-  container.appendChild(text);
-  const input = document.createElement('input');
-  const uniqueId = 'input_' + i; // Unique ID for each input field 
-  input.setAttribute('type', 'text');
-  input.setAttribute('id', uniqueId);  
-  container.appendChild(input);
-  };
