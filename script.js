4e064f2fb5725776890e7e1895af340472b78b51
Michal Lozowski <michal.lozowski@proton.me>
2023-06-18 22:01:51 +0200

    noname

--- script.js
+++ script.js
@@ -36,6 +36,6 @@ autoWidthInputs.forEach(input => {
   input.addEventListener('input', () => {
     const inputLength = input.value.length;
     let width = inputLength * 10; 
-    input.style.width = width + 'px'; // Set the width of the input field
+    input.style.width = width + '100px';
   });
 });
