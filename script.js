41ab50a9c527caeed2d6d15374e75b52a53f1b69
Michal Lozowski <michal.lozowski@proton.me>
2023-06-20 01:32:49 +0200

    noname

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ fetch('questions.txt')
         input.setAttribute('type', 'text');
         input.setAttribute('id', uniqueId);
         input.setAttribute('class', "answerField"); 
-        if (i = 0) input.setAttribute('autofocus');
         spantext.appendChild(input);      
         };
       container.appendChild(spantext);
@@ -23,33 +22,10 @@ fetch('questions.txt')
   };
 
   });
-/*
+
 fetch('answers.txt')
   .then(response => response.text())
   .then(answersText => {
     const lines = answersText.split('\n');
     const answers = lines.map(line => line.split(','));
-    
-    
-    function checkInput(event, index) {
-      if (event.key === 'Enter') {
-        const inputField = document.getElementById('input_' + i);
-        const nextInputField = document.getElementById('input_' + (i+1));
-        
-        const inputValue = event.target.value.trim();
-
-        event.preventDefault();
-        nextInputField.focus();
-        
-        let isCorrect = false;
-        for (let i = 0; i < answers[index]; i++) {
-          for (let j = 0; j < answers[index].length; j++)
-            if (inputValue === answers[index]) isCorrect = true;
-        };
-        if (isCorrect) inputField.style.border = "2px solid green";  
-      };
-  };
-
-
   });
-*/
