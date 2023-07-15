a6037431f1e09a6288e4ff46f06dedb4baaf9f1f
Michal Lozowski <michal.lozowski@proton.me>
2023-07-16 00:19:51 +0200

    noname

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-fetch('questions.txt')
+/* fetch('questions.txt')
   .then(response => response.text())
   .then(questionsText => {
+    
     questionsText = questionsText.replace(/\n/g,"<br>")
     const questions = questionsText.trim().split('_');
       
     container = document.getElementById('test-container');
+    
     for (let i = 0; i < questions.length; i++) {
-      
       const spantext = document.createElement('span');
       spantext.innerHTML = questions[i];
       if (i < questions.length - 1) {
@@ -18,8 +19,8 @@ fetch('questions.txt')
         spantext.appendChild(input);      
         };
       container.appendChild(spantext);
-      
   };
+
 
   });
 
@@ -29,17 +30,64 @@ fetch('answers.txt')
   .then(response => response.text())
   .then(answersText => {
     answers = answersText.split('\n');
-  });
+      });
+*/
 
-var answerFields;
+async function mainclown() {
 
-document.addEventListener("DOMContentLoaded", () => {
-answerFields = document.querySelectorAll(".answerField");
-answerFields.forEach(input => {
-  input.addEventListener("keyup", event => {
-    if (event.key === "Enter") {
-       event.target.style.backgroundColor = "green";
-    };
+  const responsequestions = await fetch ("questions.txt");
+  const questionsText = await responsequestions.text();
+  const responseanswers = await fetch ("answers.txt");
+  const answersText = await responseanswers.text();
+
+  const answers = answersText.split('\n');
+  const questions = questionsText.replace(/\n/g, "<br>").trim().split('_');
+        
+  container = document.getElementById('test-container');
+    
+  for (let i = 0; i < questions.length; i++) {
+    const spantext = document.createElement('span');
+    spantext.innerHTML = questions[i];
+    if (i < questions.length - 1) {
+      const input = document.createElement('input');
+      const uniqueId = i;  
+      input.setAttribute('type', 'text');
+      input.setAttribute('id', uniqueId);
+      input.setAttribute('class', "answerField"); 
+      spantext.appendChild(input);      
+      };
+    container.appendChild(spantext);
+  };
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const answerFields = document.querySelectorAll(".answerField");
+    answerFields.forEach(input => {
+      input.addEventListener("keyup", event => {
+        if (event.key === "Enter") {
+           event.target.style.backgroundColor = "green";
+        };
+      });
+    });
   });
-});
-});
+    
+}
+
+mainclown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
