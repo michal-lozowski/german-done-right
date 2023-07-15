7e0c49bc44a274e2a0e9ab69200529178a12c779
Michal Lozowski <michal.lozowski@proton.me>
2023-07-16 00:45:43 +0200

    noname

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-/* fetch('questions.txt')
-  .then(response => response.text())
-  .then(questionsText => {
-    
-    questionsText = questionsText.replace(/\n/g,"<br>")
-    const questions = questionsText.trim().split('_');
-      
-    container = document.getElementById('test-container');
-    
-    for (let i = 0; i < questions.length; i++) {
-      const spantext = document.createElement('span');
-      spantext.innerHTML = questions[i];
-      if (i < questions.length - 1) {
-        const input = document.createElement('input');
-        const uniqueId = i;  
-        input.setAttribute('type', 'text');
-        input.setAttribute('id', uniqueId);
-        input.setAttribute('class', "answerField"); 
-        spantext.appendChild(input);      
-        };
-      container.appendChild(spantext);
-  };
-
-
-  });
-
-var answers;
-
-fetch('answers.txt')
-  .then(response => response.text())
-  .then(answersText => {
-    answers = answersText.split('\n');
-      });
-*/
-
 async function mainclown() {
 
   const responsequestions = await fetch ("questions.txt");
@@ -57,20 +22,16 @@ async function mainclown() {
       spantext.appendChild(input);      
       };
     container.appendChild(spantext);
-  };
-
-    
+  };   
 }
 
 mainclown()
   .then(() => {
-
-      console.log("yay");
       const answerFields = document.querySelectorAll(".answerField");
       answerFields.forEach(input => {
         input.addEventListener("keyup", event => {
-          if (event.key === "Enter") {
-             event.target.style.backgroundColor = "green";
+          if (event.target.value.trim() === answers[event.target.id]) {
+            event.target.style.backgroundColor = "green";
           };
         });
       });
