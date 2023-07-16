f9758aa8384f4575a6899012991c57bc48a4b710
Michal Lozowski <michal.lozowski@proton.me>
2023-07-16 16:25:20 +0200

    noname

--- beginner/script.js
+++ beginner/script.js
@@ -9,19 +9,21 @@ async function mainclown() {
     const germanContainer = document.getElementById('german-container');
     const russianContainer = document.getElementById('russian-container');
     
-    const germanParagraphs = german.split("\n").replace(/\n/g, "<br>");
-    const russianParagraphs = russian.split("\n").replace(/\n/g, "<br>");
+    const germanParagraphs = german.split("\n");
+    const russianParagraphs = russian.split("\n");
 
     germanParagraphs.forEach((paragraph) => {
         const paragraphElement = document.createElement("p");
-        const paragraphText = document.createTextNode(paragraph);
+        let paragraphText = paragraph.replace(/\n/g, "<br>");
+        paragraphText = document.createTextNode(paragraph);
         paragraphElement.appendChild(paragraphText);
         germanContainer.appendChild(paragraphElement);
     })
 
     russianParagraphs.forEach((paragraph) => {
         const paragraphElement = document.createElement("p");
-        const paragraphText = document.createTextNode(paragraph);
+        let paragraphText = paragraph.replace(/\n/g, "<br>");
+        paragraphText = document.createTextNode(paragraph);
         paragraphElement.appendChild(paragraphText);
         russianContainer.appendChild(paragraphElement);
     })
