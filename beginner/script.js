de88c275957ea1bf0b5fff931c4bdbeed96188b2
Michal Lozowski <michal.lozowski@proton.me>
2023-07-16 21:34:46 +0200

    noname

--- beginner/script.js
+++ beginner/script.js
@@ -9,8 +9,8 @@ async function mainclown() {
     const germanContainer = document.getElementById('german-container');
     const russianContainer = document.getElementById('russian-container');
     
-    const germanParagraphs = german.split("\n\n");
-    const russianParagraphs = russian.split("\n\n");
+    const germanParagraphs = german.split("\r\n\r\n");
+    const russianParagraphs = russian.split("\r\n\r\n");
 
     let germanIdCounter = 0;
     germanParagraphs.forEach((paragraph) => {
@@ -18,9 +18,20 @@ async function mainclown() {
         const paragraphText = paragraph.replace(/\n/g, "<br>");
         paragraphElement.innerHTML = paragraphText;
         paragraphElement.id = "german-paragraph-" + germanIdCounter;
-        paragraphElement.setAttribute("class", "german-paragraph"); 
+        paragraphElement.classList.add("german-paragraph"); 
         germanIdCounter++;
         germanContainer.appendChild(paragraphElement);
+    })
+
+    let russianIdCounter = 0;
+    russianParagraphs.forEach((paragraph) => {
+        const paragraphElement = document.createElement("p");
+        const paragraphText = paragraph.replace(/\n/g, "<br>");
+        paragraphElement.innerHTML = paragraphText;
+        paragraphElement.id = "russian-paragraph-" + russianIdCounter;
+        paragraphElement.classList.add("russian-paragraph");
+        russianIdCounter++;
+        russianContainer.appendChild(paragraphElement);
     })
 
 
@@ -28,3 +39,18 @@ async function mainclown() {
 }
   
 mainclown()
+.then(() => {
+    const russianParagraphs = document.querySelectorAll(".russian-paragraph");
+    russianParagraphs.forEach(input => {
+        input.addEventListener("click", event => {
+        const clickedRussianParagraphId = event.target.id;
+        const germanParagraphId = clickedRussianParagraphId.replace("russian", "german");
+        console.log(germanParagraphId)
+        const germanParagraph = document.getElementById(germanParagraphId);
+        if (germanParagraph.style.visibility === "visible") {
+            germanParagraph.style.visibility = "hidden"
+        } else germanParagraph.style.visibility = "visible"
+      });
+    });
+  });
+
