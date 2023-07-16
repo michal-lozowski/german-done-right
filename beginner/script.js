1810cf05f620f79a4be0ea77e92f5a8e77084ae7
Michal Lozowski <michal.lozowski@proton.me>
2023-07-16 03:06:49 +0200

    noname

--- beginner/script.js
+++ beginner/script.js
@@ -4,17 +4,16 @@ async function mainclown() {
     const responsegerman = await fetch ("german.txt");
     const germantext = await responsegerman.text();
     const responserussian = await fetch ("russian.txt");
-    const russiantext = await responserussian.text();   
+    const russiantext = await responserussian.text();
+    
+    germancontainer = document.getElementById('german-container');
+    russiancontainer = document.getElementById('russian-container');
+    
+    const germanTextNode = document.createTextNode(germantext);
+    const russianTextNode = document.createTextNode(russiantext);
+
+    germancontainer.appendChild(germanTextNode);
+    russiancontainer.appendChild(russianTextNode);   
 }
   
 mainclown()
-    .then (() => {      
-        germancontainer = document.getElementById('german-container');
-        russiancontainer = document.getElementById('russian-container');
-        
-        const germanTextNode = document.createTextNode(germantext);
-        const russianTextNode = document.createTextNode(russiantext);
-
-        germancontainer.appendChild(germanTextNode);
-        russiancontainer.appendChild(russianTextNode);
-})
