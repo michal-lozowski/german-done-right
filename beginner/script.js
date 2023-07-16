242c05012e97032ada75d1ac0c5491a3bf1bfa7a
Michal Lozowski <michal.lozowski@proton.me>
2023-07-16 03:35:45 +0200

    noname

--- beginner/script.js
+++ beginner/script.js
@@ -2,13 +2,16 @@
 async function mainclown() {
 
     const responsegerman = await fetch ("german.txt");
-    const germantext = await responsegerman.text();
+    const german = await responsegerman.text();
     const responserussian = await fetch ("russian.txt");
-    const russiantext = await responserussian.text();
+    const russian = await responserussian.text();
     
     germancontainer = document.getElementById('german-container');
     russiancontainer = document.getElementById('russian-container');
     
+    const germantext = german.replace(/\n/g, "<br>").trim();
+    const russiantext = russian.replace(/\n/g, "<br>").trim();
+  
     const germanTextNode = document.createTextNode(germantext);
     const russianTextNode = document.createTextNode(russiantext);
 
