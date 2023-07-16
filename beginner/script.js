787b8d55644e8d60735ea878a38d2f636d52123e
Michal Lozowski <michal.lozowski@proton.me>
2023-07-16 02:55:11 +0200

    noname

---
+++ beginner/script.js
@@ -0,0 +1,13 @@
+
+async function mainclown() {
+
+    const responsegerman = await fetch ("german.txt");
+    const germantext = await responsegerman.text();
+    const responserussian = await fetch ("russian.txt");
+    const russiantext = await responserussian.text();
+  
+    germancontainer = document.getElementById('german-container');
+    russiancontainer = document.getElementById('russian-container');
+}
+  
+mainclown()
