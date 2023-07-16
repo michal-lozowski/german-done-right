acef35df693ee787e413e38bc7ecde73afd3e535
Michal Lozowski <michal.lozowski@proton.me>
2023-07-16 16:36:07 +0200

    noname

--- beginner/script.js
+++ beginner/script.js
@@ -12,17 +12,21 @@ async function mainclown() {
     const germanParagraphs = german.split("\n\n");
     const russianParagraphs = russian.split("\n\n");
 
+    let germanIdCounter = 0;
     germanParagraphs.forEach((paragraph) => {
         const paragraphElement = document.createElement("p");
         const paragraphText = paragraph.replace(/\n/g, "<br>");
         paragraphElement.innerHTML = paragraphText;
+        paragraphElement.id = "german-paragraph-" + germanIdCounter;
         germanContainer.appendChild(paragraphElement);
     })
 
+    let russianIdCounter = 0;
     russianParagraphs.forEach((paragraph) => {
         const paragraphElement = document.createElement("p");
         const paragraphText = paragraph.replace(/\n/g, "<br>");
-        paragraphElement.innerHTML = paragraphText
+        paragraphElement.innerHTML = paragraphText;
+        paragraphElement.id = "russian-paragraph-" + russianIdCounter;
         russianContainer.appendChild(paragraphElement);
     })
 
