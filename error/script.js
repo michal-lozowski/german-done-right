8c4204e2cb2388a69e61363e9aa37d8ab75dd5db
Michal Lozowski <michal.lozowski@proton.me>
2023-07-22 03:18:39 +0200

    noname

--- error/script.js
+++ error/script.js
@@ -30,7 +30,7 @@ handleKeyPress ("m", "Uncaught LexicalError: модальный глагол")
 
 document.addEventListener('keypress', function(event) {
   if (event.key === "z") {
-    const elementsArray = Array.from(document.getElementsByClassName(mainList));
+    const elementsArray = Array.from(document.getElementsByClassName("mainList"));
     elementsArray.forEach(element => element.remove());
   }
 })
