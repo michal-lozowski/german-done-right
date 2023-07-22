74ea7b86d4703799ba05c09bdaf3052b3e575b58
Michal Lozowski <michal.lozowski@proton.me>
2023-07-22 02:36:39 +0200

    noname

--- error/script.js
+++ error/script.js
@@ -1,24 +1,22 @@
 const mainList = document.getElementsByClassName('mainList');
 
 document.addEventListener('keypress', function(event) {
-  if ((event.key === "o") && (!document.getElementById('o'))) {
-    const newListItem = document.createElement('li');
-    newListItem.textContent = 'Uncaught SyntaxError: порядок слов';
-    newListItem.setAttribute('id', 'o');
-    mainList[0].appendChild(newListItem)
-  } else if (document.getElementById('o')) {document.getElementById('o').remove()};
+  if (event.key === "o") {
+    if (document.getElementById('o')) {document.getElementById('o').remove()} else {
+      const newListItem = document.createElement('li');
+      newListItem.textContent = 'Uncaught SyntaxError: порядок слов';
+      newListItem.setAttribute('id', 'o');
+      mainList[0].appendChild(newListItem);
+      }
+  }
 
-  if ((event.key === "a") && (!document.getElementById('a'))) {
+if (event.key === "a") {
+  if (document.getElementById('a')) {document.getElementById('a').remove()} else {
     const newListItem = document.createElement('li');
     newListItem.textContent = 'Uncaught SyntaxError: вспомогательный глагол';
     newListItem.setAttribute('id', 'a');
-    mainList[0].appendChild(newListItem)
-  } else if (document.getElementById('a')) {document.getElementById('a').remove()};
-  
-  if ((event.key === "p") && (!document.getElementById('p'))) {
-    const newListItem = document.createElement('li');
-    newListItem.textContent = 'Uncaught SyntaxError: форма глагола в Perfekt';
-    newListItem.setAttribute('id', 'p');
-    mainList[0].appendChild(newListItem)
-  } else if (document.getElementById('p')) {document.getElementById('p').remove()};
+    mainList[0].appendChild(newListItem);
+    }
+}
+
 });
