e7a7fe4313f2920e153b5357dea1b98eb53760f8
Michal Lozowski <michal.lozowski@proton.me>
2023-07-22 02:31:22 +0200

    noname

--- error/script.js
+++ error/script.js
@@ -6,19 +6,19 @@ document.addEventListener('keypress', function(event) {
     newListItem.textContent = 'Uncaught SyntaxError: порядок слов';
     newListItem.setAttribute('id', 'o');
     mainList[0].appendChild(newListItem)
-  } else {document.getElementById('o').remove()};
+  } else if (document.getElementById('o')) {document.getElementById('o').remove()};
 
   if ((event.key === "a") && (!document.getElementById('a'))) {
     const newListItem = document.createElement('li');
     newListItem.textContent = 'Uncaught SyntaxError: вспомогательный глагол';
     newListItem.setAttribute('id', 'a');
     mainList[0].appendChild(newListItem)
-  } else {document.getElementById('a').remove()};
+  } else if (document.getElementById('a')) {document.getElementById('a').remove()};
   
   if ((event.key === "p") && (!document.getElementById('p'))) {
     const newListItem = document.createElement('li');
     newListItem.textContent = 'Uncaught SyntaxError: форма глагола в Perfekt';
     newListItem.setAttribute('id', 'p');
     mainList[0].appendChild(newListItem)
-  } else {document.getElementById('p').remove()};
+  } else if (document.getElementById('p')) {document.getElementById('p').remove()};
 });
