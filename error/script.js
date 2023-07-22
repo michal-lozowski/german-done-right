1545e04a4f29d0c6993ee281be2642ff3e0b1278
Michal Lozowski <michal.lozowski@proton.me>
2023-07-22 02:56:30 +0200

    noname

--- error/script.js
+++ error/script.js
@@ -1,22 +1,21 @@
 const mainList = document.getElementsByClassName('mainList');
 
-document.addEventListener('keypress', function(event) {
-  if (event.key === "o") {
-    if (document.getElementById('o')) {document.getElementById('o').remove()} else {
-      const newListItem = document.createElement('li');
-      newListItem.textContent = 'Uncaught SyntaxError: порядок слов';
-      newListItem.setAttribute('id', 'o');
-      mainList[0].appendChild(newListItem);
+function handleKeyPress(key, errorMessage) {
+  document.addEventListener('keypress', function(event) {
+    if (event.key === key) {
+      const existingItem = document.getElementById(key);
+      
+      if (existingItem) {
+        document.getElementById(key).remove()
+      } else {
+        const newListItem = document.createElement('li');
+        newListItem.textContent = errorMessage;
+        newListItem.setAttribute('id', key);
+        mainList[0].appendChild(newListItem);
       }
-  }
-
-if (event.key === "a") {
-  if (document.getElementById('a')) {document.getElementById('a').remove()} else {
-    const newListItem = document.createElement('li');
-    newListItem.textContent = 'Uncaught SyntaxError: вспомогательный глагол';
-    newListItem.setAttribute('id', 'a');
-    mainList[0].appendChild(newListItem);
     }
+  })
 }
 
-});
+handleKeyPress ("o", "Uncaught SyntaxError: порядок слов")
+handleKeyPress ("a", "Uncaught SyntaxError: вспомогательный глагол")
