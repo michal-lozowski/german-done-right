b17830c58b4171e96bf02d8bd7d2da65c1e240a7
Michal Lozowski <michal.lozowski@proton.me>
2023-07-22 02:26:37 +0200

    noname

--- error/script.js
+++ error/script.js
@@ -6,17 +6,19 @@ document.addEventListener('keypress', function(event) {
     newListItem.textContent = 'Uncaught SyntaxError: порядок слов';
     newListItem.setAttribute('id', 'o');
     mainList[0].appendChild(newListItem)
-  } else document.getElementById('o').remove();
+  } else {document.getElementById('o').remove()};
+
   if ((event.key === "a") && (!document.getElementById('a'))) {
     const newListItem = document.createElement('li');
     newListItem.textContent = 'Uncaught SyntaxError: вспомогательный глагол';
     newListItem.setAttribute('id', 'a');
     mainList[0].appendChild(newListItem)
-  } else document.getElementById('a').remove();
+  } else {document.getElementById('a').remove()};
+  
   if ((event.key === "p") && (!document.getElementById('p'))) {
     const newListItem = document.createElement('li');
     newListItem.textContent = 'Uncaught SyntaxError: форма глагола в Perfekt';
     newListItem.setAttribute('id', 'p');
     mainList[0].appendChild(newListItem)
-  } else document.getElementById('p').remove();
+  } else {document.getElementById('p').remove()};
 });
