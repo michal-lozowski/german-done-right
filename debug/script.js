90b8dcb389a565548fd57e15e3d8614db39d3832
Michal Lozowski <michal.lozowski@proton.me>
2023-07-22 04:22:09 +0200

    noname

--- debug/script.js
+++ debug/script.js
@@ -18,13 +18,16 @@ function handleKeyPress(key, errorMessage) {
 }
 
 handleKeyPress ("o", "Uncaught SyntaxError: порядок слов")
+
 handleKeyPress ("u", "Uncaught SyntaxError: вспомогательный глагол")
 handleKeyPress ("p", "Uncaught SyntaxError: форма глагола в Perfekt")
 handleKeyPress ("v", "Uncaught SyntaxError: форма глагола")
 handleKeyPress ("w", "Uncaught SyntaxError: глагол с изменяющейся гласной")
-handleKeyPress ("f", "Uncaught SyntaxError: глагол с отделяемой приставкой")
+handleKeyPress ("f", "Uncaught SyntaxError: глагол с отделяемой приставкой"
+)
 handleKeyPress ("c", "Uncaught SyntaxError: падежи")
 handleKeyPress ("j", "Uncaught SyntaxError: склонение прилагательных")
+
 handleKeyPress ("s", "Uncaught LexicalError: произношение/форма слова")
 handleKeyPress ("n", "Uncaught LexicalError: предлог(и)")
 handleKeyPress ("m", "Uncaught LexicalError: модальный глагол")
