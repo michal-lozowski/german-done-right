d091dc2cec9c7afdcd736fed856ece888ee5da74
Michal Lozowski <michal.lozowski@proton.me>
2023-07-26 02:55:09 +0200

    noname

--- js/artikel.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("artikel-iframe").onload = function() {
-    var cssLink = document.createElement("link");
-    cssLink.rel = "stylesheet";
-    cssLink.href = "../css/artikel.css";
-    document.head.appendChild(cssLink);
-}
