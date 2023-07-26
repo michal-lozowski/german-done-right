fd2b0a7fa64e65949519c4797712f4c8e517e2a9
Michal Lozowski <michal.lozowski@proton.me>
2023-07-27 00:42:13 +0200

    noname

--- js/artikel.js
+++ js/artikel.js
@@ -1,6 +1,32 @@
-fetch('../artikel/table.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('table-div').innerHTML = data;
-})
+async function mainclown() {
+
+const tableResponse = await fetch ("table.html");
+const tableData = await tableResponse.text();
+
+document.getElementById('table-div').innerHTML = tableData;
+
+const textResponse = await fetch ("text.txt")
+let textData = await textResponse.text();
+
+textData = textData.replace(/\n/g, "<br>").trim();
+
+const words = textData.split(/\s+/);
+let outputHtml = '';
+words.forEach((word) => {
+  if (word.startsWith('!')) {
+    outputHtml += `<span>${word}</span> `;
+  } else {
+    outputHtml += `${word} `;
+  }
+});
+    
+document.getElementById('text-div').innerHTML = outputHtml;
+
+}
+
+mainclown()
+  .then(() => {
+
+    });
+  
 
