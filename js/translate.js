4e64e6e62ca0ccd993ba9cae368c981a33f0795e
Michal Lozowski <michal.lozowski@proton.me>
2023-09-12 23:20:05 +0300

    no commit message

--- js/translate.js
+++ js/translate.js
@@ -1,14 +1,14 @@
 
 async function mainclown() {
 
-    const responseGerman = await fetch ("german.txt");
+    const responseGerman = await fetch("german.txt");
     const german = await responseGerman.text();
-    const responseRussian = await fetch ("russian.txt");
+    const responseRussian = await fetch("russian.txt");
     const russian = await responseRussian.text();
-    
+
     const germanContainer = document.getElementById('german-container');
     const russianContainer = document.getElementById('russian-container');
-    
+
     const germanParagraphs = german.split("\n\n");
     const russianParagraphs = russian.split("\n\n");
 
@@ -18,7 +18,7 @@ async function mainclown() {
         const paragraphText = paragraph.replace(/\n/g, "<br>");
         paragraphElement.innerHTML = paragraphText;
         paragraphElement.id = "german-paragraph-" + germanIdCounter;
-        paragraphElement.classList.add("german-paragraph"); 
+        paragraphElement.classList.add("german-paragraph");
         germanIdCounter++;
         germanContainer.appendChild(paragraphElement);
     })
@@ -37,20 +37,37 @@ async function mainclown() {
 
 
 }
-  
+
 mainclown()
-.then(() => {
-    const russianParagraphs = document.querySelectorAll(".russian-paragraph");
-    russianParagraphs.forEach(input => {
-        input.addEventListener("click", event => {
-        const clickedRussianParagraphId = event.target.id;
-        const germanParagraphId = clickedRussianParagraphId.replace("russian", "german");
-        console.log(germanParagraphId)
-        const germanParagraph = document.getElementById(germanParagraphId);
-        if (germanParagraph.style.visibility === "visible") {
-            germanParagraph.style.visibility = "hidden"
-        } else germanParagraph.style.visibility = "visible";
-      });
+    .then(() => {
+        const russianParagraphs = document.querySelectorAll(".russian-paragraph");
+        russianParagraphs.forEach(input => {
+            input.addEventListener("click", event => {
+                const clickedRussianParagraphId = event.target.id;
+                const germanParagraphId = clickedRussianParagraphId.replace("russian", "german");
+                console.log(germanParagraphId)
+                const germanParagraph = document.getElementById(germanParagraphId);
+                if (germanParagraph.style.visibility === "visible") {
+                    germanParagraph.style.visibility = "hidden"
+                } else germanParagraph.style.visibility = "visible";
+            });
+        });
+
+        function equalizeParagraphSizes() {
+            for (i = 0; i < russianParagraphs.length; i++) {
+                const russianHeight = document.getElementById("russian-paragraph-" + i).getBoundingClientRect().height;
+                const germanHeight = document.getElementById("german-paragraph-" + i).getBoundingClientRect().height;
+
+                if (russianHeight > germanHeight) {
+                    document.getElementById("german-paragraph-" + i).style.height = russianHeight + "px";
+                } else {
+                    document.getElementById("russian-paragraph-" + i).style.height = germanHeight + "px";
+                }
+            }
+        }
+
+        equalizeParagraphSizes()
+        window.onresize = equalizeParagraphSizes;
+
     });
-  });
 
