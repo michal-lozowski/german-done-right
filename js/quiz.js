447918d1b2e956a1586c7661f854cbaff4713fcb
Michal Lozowski <michal.lozowski@proton.me>
2024-02-26 13:05:26 +0100

    quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,33 +1,3 @@
-
-
-async function mainclown() {
-
-    const baseURL = window.location.href.endsWith('/') ? window.location.href : window.location.href + '/';
-
-    const responseQuestions = await fetch(baseURL + "questions.txt");
-    let questionsText = await responseQuestions.text();
-
-    answerFound = false
-    answerCounter = -1
-    for (let i = 0; i < questionsText.length; i++) {
-        if (questionsText[i - 1] === "(") {
-            answerFound = true
-            dollarCounter = 0
-            answerCounter++
-        }
-        if (questionsText[i] === ")") answerFound = false
-        if (answerFound) {
-            if (answers.length === answerCounter) answers.push("")
-            answers[answerCounter] = answers[answerCounter] + questionsText[i]
-        }
-    }
-    questionsText = questionsText.replace(/\(.+\)/g, "")
-    questions = questionsText.split("\n");
-
-
-}
-
-
 let answers = []
 let questions = []
 
@@ -56,94 +26,46 @@ let won = false
 let lost = false
 let gameOver = false
 
-let fuckingCounter = 180
+let delayCounter = 180
 let score = 0
 
-async function setup() {
-    await mainclown()
-
-
-    canvasW = windowWidth / 1.5
-    canvasH = windowHeight / 1.5
-    createCanvas(canvasW, canvasH)
-
-    getQandA()
-
+async function mainclown() {
+    const baseURL = window.location.href.endsWith('/') ? window.location.href : window.location.href + '/'
+    const responseVocab = await fetch(baseURL + "vocab.txt")
+    vocabText = await responseVocab.text()
 }
 
 
-async function draw() {
-
-    clear()
-    background(230)
-
-    textSize(18)
-    text("Fragen erfolgreich beantwortet: " + score, 20, canvasH - 20)
-
-    if (lost === true || won === true) {
-        if (lost === true) {
-            background(200, 0, 0)
-            textSize(20)
-            text("Pech gehabt...", canvasW / 3, canvasH / 6)
-        }
-        if (won === true) {
-            background(0, 200, 0)
-            textSize(20)
-            text("Erfolg...", canvasW / 3, canvasH / 6)
-        }
-        fuckingCounter--
-        if (fuckingCounter === 0) {
-            if (won === true) score++
-            lost = false
-            won = false
-            q = null
-            aCorrect = null
-            a1 = null
-            a2 = null
-            positions = []
-            fuckingCounter = 180
-            getQandA()
-        }
-    }
-
-    aCorrect.show()
-    a1.show()
-    a2.show()
-
-    if (wasReleased) {
-        q.xSpeed = diffX
-        q.ySpeed = diffY
-        wasReleased = false
-    }
-
-    q.update()
-    q.show()
-
-    if (isDrawing) {
-        line(mouseStartX, mouseStartY, mouseEndX, mouseEndY);
-    }
-
-    if (collision(q, aCorrect)) won = true
-    if (collision(q, a1) || collision(q, a2)) lost = true
-
+function sortQandA() {
+    questions = vocabText.split("\n")
+    questions = questions.filter(question => question.length > 4)
+    questions = questions.map(question => {
+        let separatorIndex = question.search(" - ")
+        let answer = question.slice(0, separatorIndex).trim()
+        answers.push(answer)
+        return (question.slice(separatorIndex + 3))
+    })
 }
 
 
 function getQandA() {
-
     do {
         randomQuestionId = floor(random(questions.length))
     } while (shownQuestions.includes(randomQuestionId))
     shownQuestions.push(randomQuestionId)
 
     do {
-    randomAnswerIdA = floor(random(randomQuestionId - 5, randomQuestionId + 5))
-    randomAnswerIdB = floor(random(randomQuestionId - 5, randomQuestionId + 5))
-    if (randomAnswerIdA < 0) randomAnswerIdA = 0
-    if (randomAnswerIdB < 0) randomAnswerIdB = 0
-    if (randomAnswerIdA > questions.length - 1) randomAnswerIdA = questions.length 
-    if (randomAnswerIdB > questions.length - 1) randomAnswerIdB = questions.length
-    } while (randomAnswerIdA === randomQuestionId || randomAnswerIdB === randomQuestionId)
+        randomAnswerIdA = floor(random(randomQuestionId - 3, randomQuestionId + 3))
+        randomAnswerIdB = floor(random(randomQuestionId - 3, randomQuestionId + 3))
+        if (randomAnswerIdA < 0) randomAnswerIdA = 0
+        if (randomAnswerIdB < 0) randomAnswerIdB = 0
+        if (randomAnswerIdA > questions.length - 1) randomAnswerIdA = questions.length - 1
+        if (randomAnswerIdB > questions.length - 1) randomAnswerIdB = questions.length - 1
+    } while (
+        randomAnswerIdA === randomQuestionId ||
+        randomAnswerIdB === randomQuestionId ||
+        randomAnswerIdA === randomAnswerIdB
+    )
 
     randomPositionCorrect = floor(random(3))
     positions.push(randomPositionCorrect)
@@ -169,6 +91,85 @@ function getQandA() {
     a2.x = canvasW / 1.5
     a2.y = canvasH / 4 + (canvasH / 4 * randomPositionB)
 }
+
+
+
+async function setup() {
+    await mainclown()
+
+
+    canvasW = windowWidth / 1.5
+    canvasH = windowHeight / 1.5
+    createCanvas(canvasW, canvasH)
+
+    sortQandA()
+    getQandA()
+
+}
+
+
+async function draw() {
+
+    clear()
+    background(230)
+
+    textSize(18)
+    text("Fragen erfolgreich beantwortet: " + score + " von insgesamt " + questions.length, 20, canvasH - 20)
+
+    if (lost === true || won === true) {
+        if (lost === true) {
+            background(200, 0, 0)
+            textSize(20)
+            text("Pech gehabt...", canvasW / 3, canvasH / 6)
+        }
+        if (won === true) {
+            background(0, 200, 0)
+            textSize(20)
+            text("Erfolg...", canvasW / 3, canvasH / 6)
+        }
+        delayCounter--
+        if (delayCounter === 0) {
+            if (won === true) score++
+            lost = false
+            won = false
+            q = null
+            aCorrect = null
+            a1 = null
+            a2 = null
+            positions = []
+            delayCounter = 180
+            getQandA()
+        }
+    }
+
+    aCorrect.show()
+    a1.show()
+    a2.show()
+
+    if (wasReleased) {
+        q.xSpeed = diffX
+        q.ySpeed = diffY
+        wasReleased = false
+    }
+
+    q.update()
+    q.show()
+
+    if (isDrawing) {
+        line(mouseStartX, mouseStartY, mouseEndX, mouseEndY);
+        let angle = atan2(mouseEndY - mouseStartY, mouseEndX - mouseStartX);
+        push();
+        translate(mouseEndX, mouseEndY);
+        rotate(angle);
+        triangle(-20, 8, 0, 0, -20, -8);
+        pop();
+    }
+
+    if (collision(q, aCorrect)) won = true
+    if (collision(q, a1) || collision(q, a2)) lost = true
+
+}
+
 
 function collision(a, b) {
     if (
@@ -200,35 +201,28 @@ function Question(textToShow) {
     this.x = canvasW / 6
     this.y = canvasH / 2
 
-    textSize(24)
-
     this.xSpeed = 0
     this.ySpeed = 0
 
-    /* let dragA = random(-0.1, -0.25)
-    let dragB = random(0.1, 0.25)
-    let chooser = floor(random(2))
-    if (chooser === 0) this.drag = dragA * 0.1
-    if (chooser === 1) this.drag = dragB * 0.1 */
-
     this.update = function () {
 
-        if (this.ySpeed !== 0) {
-            this.ySpeed += 0.025
-            /* this.ySpeed += this.drag
-            this.xSpeed += this.drag */
-            console.log(this.drag)
-        }
+
+        /* if (Math.abs(this.ySpeed) > 0.2) */ this.ySpeed += -this.ySpeed * 0.03
+        if (Math.abs(this.xSpeed) > 0.5) this.xSpeed += -this.xSpeed * 0.03
 
         this.x += this.xSpeed
-        this.y += this.ySpeed
+        this.y += this.ySpeed * 0.5
 
-        if ((this.x <= 10) || (this.x >= canvasW - 100) ||
-            ((this.y <= 10) || (this.y >= canvasH - 50))) {
+        if ((this.x <= 10) || (this.x >= canvasW - 100)) {
+            this.ySpeed = -this.ySpeed
             this.xSpeed = -this.xSpeed
+        }
+
+        if ((this.y <= 10) || (this.y >= canvasH - 50)) {
             this.ySpeed = -this.ySpeed
         }
-        
+
+
         if ((this.x <= 1) || (this.x >= canvasW - 10) ||
             ((this.y <= 1) || (this.y >= canvasH - 5))) {
             this.x = canvasW / 5
@@ -243,6 +237,7 @@ function Question(textToShow) {
 
     this.show = function () {
         textSize(24)
+        if (textToShow.length > 18) textSize(20)
         fill(0)
         text(textToShow, this.x, this.y)
         this.bl = [this.x, this.y + textDescent()];
@@ -263,6 +258,7 @@ function Answer(textToShow) {
     this.show = function () {
         fill(0)
         textSize(24)
+        if (textToShow.length > 18) textSize(20)
         text(textToShow, this.x, this.y)
         this.bl = [this.x, this.y + textDescent()];
         this.br = [this.x + textWidth(textToShow), this.y + textDescent()];
@@ -289,8 +285,12 @@ function mouseDragged() {
 function mouseReleased() {
     wasReleased = true
     isDrawing = false
-    diffX = (mouseEndX - mouseStartX) / 100
-    diffY = (mouseEndY - mouseStartY) / 100
+    diffX = (mouseEndX - mouseStartX) / 80
+    diffY = (mouseEndY - mouseStartY) / 80
+    if (!mouseEndX || !mouseEndY) {
+        diffX = 0
+        diffY = 0
+    }
     mouseStartX = undefined
     mouseStartY = undefined
     mouseEndX = undefined
